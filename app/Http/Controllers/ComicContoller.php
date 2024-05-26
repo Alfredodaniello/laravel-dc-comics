7d7fc010856f9e629e2e8033ee4819441f6940fb
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-26 14:54:14 +0200

    feat: add comic with fill

--- app/Http/Controllers/ComicContoller.php
+++ app/Http/Controllers/ComicContoller.php
@@ -43,13 +43,7 @@ class ComicContoller extends Controller
     {
         $formData = $request->all();
         $newComic = new Comic();
-        $newComic->title = $formData['title'];
-        $newComic->description = $formData['description'];
-        $newComic->thumb = $formData['thumb'];
-        $newComic->price = $formData['price'];
-        $newComic->series = $formData['series'];
-        $newComic->sale_date = $formData['sale_date'];
-        $newComic->type = $formData['type'];
+        $newComic->fill($formData);
         $newComic->save();
 }
 
