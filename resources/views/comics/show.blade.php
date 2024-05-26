5f027d7b97e2f4dc680bbc697832eab2da78ec8c
Alfredodaniello <alfredo_daniello@libero.it>
2024-05-26 12:04:38 +0200

    feat: add single comic page

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>{{$comic->title}}</h1>
+    <h1 class="text-center">{{$comic->title}}</h1>
+    <div class="card my-card my-3">
+        <div class="card-body">
+            <div>
+                <img src={{$comic->thumb}} alt="">
+            </div>
+            <div class="title">{{$comic->title}}</div>
+            <div class="description my-3">{{$comic->description}}</div>
+            <h5>{{$comic->series}}</h5>
+            <h6>Prezzo: {{$comic->price}}</h6>
+        </div>
+    </div>
 @endsection
